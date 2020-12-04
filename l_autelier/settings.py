ab5cfc227e342fbb4ec0732704118e798d1960b6
Antonio-dev7 <antonio.emk28@gmail.com>
2020-12-04 00:38:10 +0000

    homepage almost done.

--- l_autelier/settings.py
+++ l_autelier/settings.py
@@ -150,3 +150,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# final step add a couple of settings to link up our static and media files
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
