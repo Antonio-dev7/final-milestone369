119771f63325ddb981760d51d183e13e19a5462d
Antonio-dev7 <antonio.emk28@gmail.com>
2020-12-04 09:59:51 +0000

    product app, models, fixtures, debugged my fixtures

--- l_autelier/settings.py
+++ l_autelier/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'home',                     # added home to app.
+    'products',                 # added home to app.
 ]
 
 MIDDLEWARE = [
